a6465d10f531b9433a9845b8de6c7b7f0520e0cf
rds80 <rippleshah@msn.com>
2018-10-25 06:48:02 -0400

    Created a fake Weather Service and updated tests

--- local-weather-app/src/app/app.component.spec.ts
+++ local-weather-app/src/app/app.component.spec.ts
@@ -2,6 +2,7 @@ import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { CurrentWeatherComponent } from './current-weather/current-weather.component';
 import { WeatherService } from './weather/weather.service';
+import { WeatherServiceFake } from './weather/weather.service.fake';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -10,7 +11,10 @@ describe('AppComponent', () => {
         AppComponent, CurrentWeatherComponent
       ],
       providers: [
-        WeatherService
+        {
+          provide: WeatherService,
+          useClass: WeatherServiceFake
+        }
       ],
     }).compileComponents();
   }));
@@ -21,16 +25,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'local-weather-app'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('local-weather-app');
-  });
-
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to local-weather-app!');
+    expect(compiled.querySelector('h1').textContent).toContain('LocalCast Weather');
   });
 });
